1505f78016a9a2f8ba5fce85e20915a48b5bea06
cxcatesc <costin@axl.media>
2020-03-31 23:31:23 +0300

    Created components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {App} from './containers';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App intro="Welcome to react!"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
