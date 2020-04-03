4c2438ed3dd566caac1c519f672e20c3ccef18ff
cxcatesc <costin@axl.media>
2020-04-03 22:59:58 +0300

    added redux to the project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {CardList, Scroller, SearchBox} from '../components';
 import '../css/main.css';
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => ( {searchField: state.searchField} );
+
+const mapDispatch = dispatch => ( { onSearchChange: event => dispatch(setSearchField(event.target.value)) } );
+
 
 class App extends React.Component {
-    constructor() {
-      super();
+    constructor(props) {
+      super(props);
       this.state = {
         error: null,
         isLoaded: false,
         items: [],
-        query : ''
       };
     }
-
-    onSearchChange = (event) => {
-        this.setState({
-            query: event.target.value
-        })
-    }
+    
   
     componentDidMount() {
       fetch(process.env.REACT_APP_JSON_PLACEHOLDER_URL)
@@ -39,9 +40,9 @@ class App extends React.Component {
     }
   
     render() {
-        
+      const { searchField, onSearchChange} = this.props;  
       const filteredRobots = this.state.items.filter( robot => {
-        return robot.name.toLowerCase().includes(this.state.query.toLowerCase())
+        return robot.name.toLowerCase().includes(searchField.toLowerCase())
       })
       const { error, isLoaded } = this.state;
       if (error) {
@@ -49,18 +50,19 @@ class App extends React.Component {
       } else if (!isLoaded) {
         return <div>Loading...</div>;
       } else {
-        return (<div class="container-fluid">
-                    <header className="header"><h1>ROBOLIST</h1></header>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroller > 
-                        <CardList robots={ filteredRobots } />
-                    </Scroller>
-                    <hr />
-                    <footer className="footer">Costin Catescu © 2020</footer>
-                </div>
+        return (
+          <div className="container-fluid">
+            <header className="header"><h1>ROBOLIST</h1></header>
+            <SearchBox searchChange={onSearchChange} />
+            <Scroller > 
+                <CardList robots={ filteredRobots } />
+            </Scroller>
+            <hr />
+            <footer className="footer">Costin Catescu © 2020</footer>
+        </div>
         );
       }
     }
 }
 
-  export default App;
+  export default connect(mapStateToProps, mapDispatch)(App);
