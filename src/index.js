80aa454474eea1b939a10d9450c53933f48f6b6e
cxcatesc <costin@axl.media>
2020-04-03 22:58:52 +0300

    added react to the project

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider} from 'react-redux';
+import { createStore } from 'redux';
 import {App} from './containers';
 import * as serviceWorker from './serviceWorker';
+import { searchRobots } from './reducers';
+
+
+const store = createStore(searchRobots)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App intro="Welcome to react!"/>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
