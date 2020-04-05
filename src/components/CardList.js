72bc085c26a58e769e291a13a4899decb072e751
cxcatesc <costin@axl.media>
2020-04-05 17:39:59 +0300

    added me as a robot :)

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,6 +3,14 @@ import Card from './Card';
 
 const CardList = ({robots, searchItems}) => {
 
+    robots.unshift({ 
+        "id": 11,
+        "name": "Costin Catescu",
+        "username": "Costin",
+        "email": "costin.catescu@gmail.com",
+        }
+    );
+
     let filteredRobots = robots.map((item) => (<Card robot={ item } key={item.id} />));
     return (<
             div className="cardlist">
